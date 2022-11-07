250a398f5592ba23c3b68a7dcbf7ac53d74f7ddf
me0giazodoi <110475358+me0giazodoi@users.noreply.github.com>
2022-11-07 16:28:54 +0700

    update MSSQL 4
    update MSSQL 4

--- CodeSQL/MSSQL 4 (Dữ liệu).sql
+++ CodeSQL/MSSQL 4 (Dữ liệu).sql
@@ -1,14 +1,18 @@
-﻿insert into SanPham(MaSoSanPham,NgaySanXuat)
-values('Z37111111','2009-12-12');
-select * from SanPham;
+﻿insert into NguoiChiuTrachNhiem(TenNguoiChiuTrachNhiem,MaNguoiChiuTrachNhiem)
+values(N'Nguyễn Văn An','987688');
+select * from NguoiChiuTrachNhiem;
 
-insert into Loai(TenLoaiSanPham,MaLoaiSanPham,MaSoSanPham)
-values(N'Máy tính sách tay Z37','Z37E','Z37111111');
+insert into Loai(TenLoaiSanPham,MaLoaiSanPham,MaNguoiChiuTrachNhiem)
+values(N'Máy tính sách tay Z37','Z37E','987688');
 select * from Loai;
 
-insert into NguoiChiuTrachNhiem(TenNguoiChiuTrachNhiem,MaNguoiChiuTrachNhiem,MaLoaiSanPham)
-values(N'Nguyễn Văn An','987688','Z37E');
-select * from NguoiChiuTrachNhiem;
+insert into SanPham(MaSoSanPham,NgaySanXuat,MaNguoiChiuTrachNhiem,MaLoaiSanPham)
+values('Z37111111','2009-12-12','987688','Z37E');
+select * from SanPham;
+
+delete from NguoiChiuTrachNhiem;
+delete from Loai;
+delete from SanPham;
 
 --Bài 4-a
 select * from Loai;
@@ -16,7 +20,6 @@ select * from Loai;
 select * from SanPham;
 --Bài 4-c
 select * from NguoiChiuTrachNhiem;
-
 --Bài 5-a
 select * from Loai order by TenLoaiSanPham asc;
 --Bài 5-b
@@ -25,16 +28,16 @@ select * from NguoiChiuTrachNhiem order by TenNguoiChiuTrachNhiem asc;
 select TenLoaiSanPham from Loai where MaLoaiSanPham like N'Z37E';
 --Bài 5-d
 select TenLoaiSanPham from Loai where MaLoaiSanPham in
-(select MaLoaiSanPham from NguoiChiuTrachNhiem where TenNguoiChiuTrachNhiem like N'Nguyễn Văn An') order by MaSoSanPham desc;
+(select MaLoaiSanPham from NguoiChiuTrachNhiem where TenNguoiChiuTrachNhiem like N'Nguyễn Văn An') order by MaLoaiSanPham desc;
 
 --Bài 6-a
 select count(*) from Loai;
 --Bài 6-b
 
 --Bài 6-c
-select * from SanPham a
-left join Loai b on a.MaSoSanPham = b.MaSoSanPham
+select * from Loai a
+left join SanPham b on a.MaLoaiSanPham = b.MaLoaiSanPham
 --Bài 6-d
-select * from SanPham a
-left join Loai b on a.MaSoSanPham = b.MaSoSanPham
-left join NguoiChiuTrachNhiem c on b.MaLoaiSanPham = c.MaLoaiSanPham
+select * from NguoiChiuTrachNhiem a
+left join Loai b on a.MaNguoiChiuTrachNhiem = b.MaNguoiChiuTrachNhiem
+left join SanPham c on b.MaLoaiSanPham = c.MaLoaiSanPham
--- CodeSQL/MSSQL 4.sql
+++ CodeSQL/MSSQL 4.sql
@@ -1,17 +1,18 @@
-create table SanPham(
-	MaSoSanPham varchar(20) not null primary key,
-	NgaySanXuat date not null check(NgaySanXuat < getdate())
+create table NguoiChiuTrachNhiem(
+	TenNguoiChiuTrachNhiem nvarchar(255) not null,
+	MaNguoiChiuTrachNhiem varchar(20) not null primary key,
 );
 create table Loai(
 	TenLoaiSanPham nvarchar(255) not null,
 	MaLoaiSanPham varchar(20) not null primary key,
-	MaSoSanPham varchar(20) not null foreign key references SanPham(MaSoSanPham)
+	MaNguoiChiuTrachNhiem varchar(20) not null foreign key references NguoiChiuTrachNhiem(MaNguoiChiuTrachNhiem)
 );
-create table NguoiChiuTrachNhiem(
-	TenNguoiChiuTrachNhiem nvarchar(255) not null,
-	MaNguoiChiuTrachNhiem varchar(20) not null primary key,
+create table SanPham(
+	MaSoSanPham varchar(20) not null primary key,
+	NgaySanXuat date not null check(NgaySanXuat < getdate()),
+	MaNguoiChiuTrachNhiem varchar(20) not null foreign key references NguoiChiuTrachNhiem(MaNguoiChiuTrachNhiem),
 	MaLoaiSanPham varchar(20) not null foreign key references Loai(MaLoaiSanPham)
 );
-drop table SanPham;
-drop table Loai;
 drop table NguoiChiuTrachNhiem;
+drop table Loai;
+drop table SanPham;
